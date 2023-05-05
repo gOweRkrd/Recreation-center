4d1ff21ddbbaff90d88353a664a9f6d71b9bda90
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-05-05 11:24:24 +0800

    Add files via upload

---
+++ Recreation center/Recreation_centerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recreation_centerApp.swift
+//  Recreation center
+//
+//  Created by Александр Косяков on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Recreation_centerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
