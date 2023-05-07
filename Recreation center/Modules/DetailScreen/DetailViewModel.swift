e9f4524ec9adfcc4301f8280cd886ddd1e04fe3a
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-05-07 19:11:30 +0700

    Added swipe location on objects

--- Recreation center/Modules/DetailScreen/DetailViewModel.swift
+++ Recreation center/Modules/DetailScreen/DetailViewModel.swift
@@ -1,7 +1,10 @@
+import CoreLocation
+import MapKit
 import SwiftUI
 
 final class DetailViewModel: ObservableObject {
     @Published var detailModel = [Object]()
+    @Published  var images: [URL: UIImage] = [:]
     
     func fetchDetail(category: Category?) {
         guard let url = URL(string: "https://rsttur.ru/api/base-app/map") else {
@@ -33,4 +36,37 @@ final class DetailViewModel: ObservableObject {
         .resume()
     }
     
+    func loadImage(for item: Object) {
+        guard let imageUrl = item.image else { return }
+        guard images[imageUrl] == nil else { return }
+        
+        URLSession.shared.dataTask(with: imageUrl) { data, _, error in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async {
+                self.images[imageUrl] = UIImage(data: data)
+            }
+        }
+        .resume()
+    }
+    
+    func openInMaps(item: Object) {
+        guard let lat = item.lat, let lon = item.lon else {
+            return
+        }
+        // 2GIS
+        if let url = URL(string: "dgis://map/?pt=\(lon),\(lat)") {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                // Handle error
+            }
+        } else {
+            // Handle error
+        }
+        // Apple Maps
+        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate))
+        mapItem.name = item.name
+        mapItem.openInMaps(launchOptions: nil)
+    }
 }
